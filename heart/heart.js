48d875e0d1fd0f1dcdf585394347cebecb2396f2
yoon <cunbn@naver.com>
2023-06-07 20:15:21 +0900

    0607

--- heart/heart.js
+++ heart/heart.js
@@ -1,5 +1,15 @@
-$(function() {
-    $(".heart").on("click", function() {
-      $(this).toggleClass("is-active");
+// $(function() {
+//     $(".heart").on("click", function() {
+//       $(this).toggleClass("is-active");
+//     });
+//   });
+
+document.addEventListener("DOMContentLoaded", function() {
+  let heartElements = document.querySelectorAll(".heart");
+  
+  for (let i = 0; i < heartElements.length; i++) {
+    heartElements[i].addEventListener("click", function() {
+      this.classList.toggle("is-active");
     });
-  });
+  }
+});
